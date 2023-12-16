25e38b77ab1fbadb847244aa31c32372765e27e0
everGreenGH <skin0011@naver.com>
2023-12-16 22:04:39 +0900

    fix: req 타입 에러 해결

--- server/src/common/type/express.d.ts
+++ server/src/common/type/express.d.ts
@@ -1,11 +1,10 @@
 import { Request as Req } from "express";
 import { Product } from "../database/entities/product.entity";
+import { Wallet } from "../database/entities/wallet.entity";
 
 declare module "express" {
     interface Request extends Req {
-        wallet?: {
-            address?: string;
-        };
+        user?: Wallet;
         product?: Product;
     }
 }
